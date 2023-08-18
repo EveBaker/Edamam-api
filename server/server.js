6d479d39fa5113eb9a65a678c454a31cb38466eb
EveBaker <evebaker0162@gmail.com>
2023-08-17 22:21:38 -0500

    adding to server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import axios from "axios";
 import cors from "cors";
 import "dotenv/config";
@@ -7,6 +7,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
+
+app.get('/recipies/:chicken', async (req, res) => {
+    const response = await axios.get(
+        `https://api.edamam.com/search?q=chicken&app.id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+    )
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening to port ${PORT}`)
